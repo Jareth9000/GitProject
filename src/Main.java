58b0d1bae2aec9dce7da01012526cc43e60a1b04
BT_1N3_04 <sample@bths.edu>
2025-11-21 10:20:55 -0500

    changed second print statement added user input

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("hello world");
-        System.out.println("hello again");
+        System.out.println("goodbye world");
         int score = 0;
+        System.out.println("Type an integer 1 to 9");
+        score += scan.nextInt();
+        System.out.println(score);
+
     }
 }
