3fb9c9baab186428fa19a07b1b9807fb890c2ed9
BT_1N3_04 <sample@bths.edu>
2025-11-25 10:12:37 -0500

    added class to test while loops

---
+++ src/WhileLoops.java
@@ -0,0 +1,10 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+
+    }
+}
